a759bbd169f814851ce687be7f21220a4736d4ff
Yihan Liu <yihan@vt.edu>
2024-08-20 00:37:54 -0400

    Update run_imps_surge.sh

--- HPC/run_imps_surge.sh
+++ HPC/run_imps_surge.sh
@@ -67,147 +67,148 @@ module load python/3.9
 ### gen_seeds $3 $4
 mkdir seeds_surge_1_pi1
 mkdir results_surge_1_pi1
-python3 gen_seeds.py 1 1 $n_simulations
+python3 gen_seeds.py 1 1 $1
 sbatch monteCarlo.slurm $n_simulations $2 1 1 4021713
 
 mkdir seeds_surge_1_pi2
 mkdir results_surge_1_pi2
-python3 gen_seeds.py 1 2 $n_simulations
+python3 gen_seeds.py 1 2 $1
 sbatch monteCarlo.slurm $n_simulations $2 1 2 4021713
 
 mkdir seeds_surge_1_pi4
 mkdir results_surge_1_pi4
-python3 gen_seeds.py 1 4 $n_simulations
+python3 gen_seeds.py 1 4 $1
 sbatch monteCarlo.slurm $n_simulations $2 1 4 4021713
 
 mkdir seeds_surge_1_pi6
 mkdir results_surge_1_pi6
-python3 gen_seeds.py 1 6 $n_simulations
+python3 gen_seeds.py 1 6 $1
 sbatch monteCarlo.slurm $n_simulations $2 1 6 4021713
 
 mkdir seeds_surge_1_pi8
 mkdir results_surge_1_pi8
-python3 gen_seeds.py 1 8 $n_simulations
+python3 gen_seeds.py 1 8 $1
 sbatch monteCarlo.slurm $n_simulations $2 1 8 4021713
 #####################################################
 
 mkdir seeds_surge_2_pi0
 mkdir results_surge_2_pi0
-python3 gen_seeds.py 2 0 $n_simulations
+python3 gen_seeds.py 2 0 $1
 sbatch monteCarlo.slurm $n_simulations $2 2 0 8041419
 
 mkdir seeds_surge_2_pi1
 mkdir results_surge_2_pi1
-python3 gen_seeds.py 2 1 $n_simulations
+python3 gen_seeds.py 2 1 $1
 sbatch monteCarlo.slurm $n_simulations $2 2 1 8041419
 
 mkdir seeds_surge_2_pi2
 mkdir results_surge_2_pi2
-python3 gen_seeds.py 2 2 $n_simulations
+python3 gen_seeds.py 2 2 $1
 sbatch monteCarlo.slurm $n_simulations $2 2 2 8041419
 
 mkdir seeds_surge_2_pi4
 mkdir results_surge_2_pi4
-python3 gen_seeds.py 2 4 $n_simulations
+python3 gen_seeds.py 2 4 $1
 sbatch monteCarlo.slurm $n_simulations $2 2 4 8041419
 
 mkdir seeds_surge_2_pi6
 mkdir results_surge_2_pi6
-python3 gen_seeds.py 2 6 $n_simulations
+python3 gen_seeds.py 2 6 $1
 sbatch monteCarlo.slurm $n_simulations $2 2 6 8041419
 
 mkdir seeds_surge_2_pi8
 mkdir results_surge_2_pi8
-python3 gen_seeds.py 2 8 $n_simulations
+python3 gen_seeds.py 2 8 $1
 sbatch monteCarlo.slurm $n_simulations $2 2 8 8041419
 #####################################################
 mkdir seeds_surge_3_pi0
 mkdir results_surge_3_pi0
-python3 gen_seeds.py 3 0 $n_simulations
+python3 gen_seeds.py 3 0 $1
 sbatch monteCarlo.slurm $n_simulations $2 3 0 6488224
 
 mkdir seeds_surge_3_pi1
 mkdir results_surge_3_pi1
-python3 gen_seeds.py 3 1 $n_simulations
+python3 gen_seeds.py 3 1 $1
 sbatch monteCarlo.slurm $n_simulations $2 3 1 6488224
 
 mkdir seeds_surge_3_pi2
 mkdir results_surge_3_pi2
-python3 gen_seeds.py 3 2 $n_simulations
+python3 gen_seeds.py 3 2 $1
 sbatch monteCarlo.slurm $n_simulations $2 3 2 6488224
 
 mkdir seeds_surge_3_pi4
 mkdir results_surge_3_pi4
-python3 gen_seeds.py 3 4 $n_simulations
+python3 gen_seeds.py 3 4 $1
 sbatch monteCarlo.slurm $n_simulations $2 3 4 6488224
 
 mkdir seeds_surge_3_pi6
 mkdir results_surge_3_pi6
-python3 gen_seeds.py 3 6 $n_simulations
+python3 gen_seeds.py 3 6 $1
 sbatch monteCarlo.slurm $n_simulations $2 3 6 6488224
 
 mkdir seeds_surge_3_pi8
 mkdir results_surge_3_pi8
-python3 gen_seeds.py 3 8 $n_simulations
+python3 gen_seeds.py 3 8 $1
 sbatch monteCarlo.slurm $n_simulations $2 3 8 6488224
 #####################################################
 mkdir seeds_surge_4_pi0
 mkdir results_surge_4_pi0
-python3 gen_seeds.py 4 0 $n_simulations
+python3 gen_seeds.py 4 0 $1
 sbatch monteCarlo.slurm $n_simulations $2 4 0 9141186
 
 mkdir seeds_surge_4_pi1
 mkdir results_surge_4_pi1
-python3 gen_seeds.py 4 1 $n_simulations
+python3 gen_seeds.py 4 1 $1
 sbatch monteCarlo.slurm $n_simulations $2 4 1 9141186
 
 mkdir seeds_surge_4_pi2
 mkdir results_surge_4_pi2
-python3 gen_seeds.py 4 2 $n_simulations
+python3 gen_seeds.py 4 2 $1
 sbatch monteCarlo.slurm $n_simulations $2 4 2 9141186
 
 mkdir seeds_surge_4_pi4
 mkdir results_surge_4_pi4
-python3 gen_seeds.py 4 4 $n_simulations
+python3 gen_seeds.py 4 4 $1
 sbatch monteCarlo.slurm $n_simulations $2 4 4 9141186
 
+###redo this for 4
 mkdir seeds_surge_4_pi6
 mkdir results_surge_4_pi6
-python3 gen_seeds.py 4 6 $n_simulations
+python3 gen_seeds.py 4 6 $1
 sbatch monteCarlo.slurm $n_simulations $2 4 6 9141186
 
 mkdir seeds_surge_4_pi8
 mkdir results_surge_4_pi8
-python3 gen_seeds.py 4 8 $n_simulations
+python3 gen_seeds.py 4 8 $1
 sbatch monteCarlo.slurm $n_simulations $2 4 8 9141186
 #####################################################
-
+### redo 2 4 8
 mkdir seeds_surge_5_pi0
 mkdir results_surge_5_pi0
-python3 gen_seeds.py 5 0 $n_simulations
+python3 gen_seeds.py 5 0 $1
 sbatch monteCarlo.slurm $n_simulations $2 5 0 8734247
 
 mkdir seeds_surge_5_pi1
 mkdir results_surge_5_pi1
-python3 gen_seeds.py 5 1 $n_simulations
+python3 gen_seeds.py 5 1 $1
 sbatch monteCarlo.slurm $n_simulations $2 5 1 8734247
 
 mkdir seeds_surge_5_pi2
 mkdir results_surge_5_pi2
-python3 gen_seeds.py 5 2 $n_simulations
+python3 gen_seeds.py 5 2 $1
 sbatch monteCarlo.slurm $n_simulations $2 5 2 8734247
 
 mkdir seeds_surge_5_pi4
 mkdir results_surge_5_pi4
-python3 gen_seeds.py 5 4 $n_simulations
+python3 gen_seeds.py 5 4 $1
 sbatch monteCarlo.slurm $n_simulations $2 5 4 8734247
 
 mkdir seeds_surge_5_pi6
 mkdir results_surge_5_pi6
-python3 gen_seeds.py 5 6 $n_simulations
+python3 gen_seeds.py 5 6 $1
 sbatch monteCarlo.slurm $n_simulations $2 5 6 8734247
 
 mkdir seeds_surge_5_pi8
 mkdir results_surge_5_pi8
-python3 gen_seeds.py 5 8 $n_simulations
+python3 gen_seeds.py 5 8 $1
 sbatch monteCarlo.slurm $n_simulations $2 5 8 8734247
