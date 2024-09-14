0daa737a06d562371e29f263b15f5df2af43f4e7
Yihan Liu <yihanl21@vt.edu>
2024-09-14 18:23:16 -0400

    Update psd.py

--- HPC/psd.py
+++ HPC/psd.py
@@ -30,7 +30,7 @@ def load_data(directory):
     wave_eta = np.hstack(wave_etas)
     seeds = np.hstack(seeds)
     
-    return t, state, wind_speed, wave_eta, seeds
+    return state, wind_speed, wave_eta
 
 def psd(time_series, time_step = 0.5, nperseg=256):
     """
@@ -65,6 +65,18 @@ def categorize_simulations_by_wind_speed(wind_speed):
     
     return wind_0_10, wind_20
 
+def categorize_extreme_surge(surge, upper_bound):
+    
+    extreme_index = []
+    
+    for i in range(surge.shape[1]):
+        max_value = np.max(state[:, i])
+        if max_value > upper_bound:
+            extreme_index.append(i)
+    
+    return extreme_index
+            
+
 def save_psd_percentiles(psd_list, file_name):
     """
     Calculates and saves the median, 25th percentile, and 75th percentile of a list of PSDs.
@@ -86,9 +98,58 @@ def save_psd_percentiles(psd_list, file_name):
                                        p37_5=p37_5, p62_5=p62_5)
 
 # Load data
-t, state, wind_speed, wave_eta, seeds = load_data("results")
+
+# standard MCMC
+state, wind_speed, wave_eta = load_data("results")
+
+# imps data
+state_pi0, wind_speed_pi0, wave_eta_pi0 = load_data("results_surge_n15_pi0")
 
 # Categorize simulations
+extreme_list = categorize_extreme_surge(state_pi0[:, 0], 9)
+
+
+psd_wind_MCMC = []
+psd_wind_pi0 = []
+psd_wind_extreme = []
+
+
+psd_wave_MCMC = []
+psd_wave_pi0 = []
+psd_wave_extreme = []
+
+psd_surge_MCMC = []
+psd_surge_pi0 = []
+psd_surge_extreme = []
+
+for i in range(wind_speed.shape[1]):
+    psd_wind_MCMC.append(psd(wind_speed[:, i]))
+    psd_wave_MCMC.append(psd(wave_eta[:, i]))
+    psd_surge_MCMC.append(psd(state[:, 0, i]))
+    
+for i in range(wind_speed_pi0.shape[1]):
+    psd_wind_pi0.append(psd(wind_speed_pi0[:, i]))
+    psd_wave_pi0.append(psd(wave_eta_pi0[:, i]))
+    psd_surge_pi0.append(psd(state_pi0[:, 0, i]))
+    
+for i in extreme_list:
+    psd_wind_extreme.append(psd(wind_speed_pi0[:, i]))
+    psd_wave_extreme.append(psd(wave_eta_pi0[:, i]))
+    psd_surge_extreme.append(psd(state_pi0[:, 0, i]))
+    
+save_psd_percentiles(psd_wind_MCMC, "psd/wind_MCMC")
+save_psd_percentiles(psd_wind_pi0, "psd/wind_pi0")
+save_psd_percentiles(psd_wind_extreme, "psd/wind_extreme")
+
+save_psd_percentiles(psd_wave_MCMC, "psd/wind_MCMC")
+save_psd_percentiles(psd_wave_pi0, "psd/wave_pi0")
+save_psd_percentiles(psd_wave_extreme, "psd/wave_extreme")
+
+save_psd_percentiles(psd_surge_MCMC, "psd/surge_MCMC")
+save_psd_percentiles(psd_surge_pi0, "psd/surge_pi0")
+save_psd_percentiles(psd_surge_extreme, "psd/surge_extreme")
+
+'''
 wind_0_10, wind_20 = categorize_simulations_by_wind_speed(wind_speed)
 
 psd_wind_0_10 = []
@@ -121,5 +182,5 @@ save_psd_percentiles(psd_wave_20, "psd/wave_20")
 
 save_psd_percentiles(psd_surge_0_10, "psd/surge_0_10")
 save_psd_percentiles(psd_surge_20, "psd/surge_20")
-
+'''
 
