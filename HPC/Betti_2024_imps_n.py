0642a0e45249976ff8aecacacebc5c6adcde450d
Yihan Liu <yihanl21@vt.edu>
2024-10-05 16:33:48 -0400

    new iteration with5000

--- HPC/Betti_2024_imps_n.py
+++ HPC/Betti_2024_imps_n.py
@@ -865,9 +865,15 @@ def main(end_time, v_w, x0, file_index, seeds, time_step = 0.05, T_s1 = 180):
     np.random.set_state(state_before)
     
     # for large scale component, load the configuration
-    random_index = np.random.randint(0, 15)
+    seeds_list = np.array([8696505, 2514970, 8041419, 8734247, 563415, 8722005, 8086850, 8399725, 1106191, 1991645, 830830, 1885232, 9141186, 2876421, 1860654])
+    sample_seed = np.random.choice(seeds_list)
+    state_before = np.random.get_state()
+    np.random.seed(sample_seed)
+    sampling_source = np.random.uniform(-np.pi, np.pi, 31) 
+    np.random.set_state(state_before)
     
-    sampling_source = np.load("imps_ite/imps_surge_ml_pi0_ite1.npy")[:, random_index]
+    #random_index = np.random.randint(0, 15)
+    #sampling_source = np.load("imps_ite/imps_surge_ml_pi0_ite1.npy")[:, random_index]
     
     # generate large-scale, build normal distribution around the loaded configuration
     state_before = np.random.get_state()                                                                                                                                                                
